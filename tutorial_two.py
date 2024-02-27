65cf932f2b8a2ad6ef74775a23bdeb5bfdb18bce
kamukwamba mulubwa <raymondmasupha@gmail.com>
2024-02-27 11:14:40 +0200

    new

--- tutorial_two.py
+++ tutorial_two.py
@@ -1,3 +1,3 @@
 print("Welocme to the file of filess")
-print("adding new print statement")
-print("To much change in one go")
+print("adding new print statement to file")
+print("kAMUKWAMBa IS COOL")
